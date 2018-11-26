2423bd48d4a533641f1b2f3987b64a36ede30257
DaWe1992 <daniel.wehner92@gmx.de>
2018-11-26 10:56:36 +0100

    started feature extraction

--- pos_tagging_crf.py
+++ pos_tagging_crf.py
@@ -52,5 +52,61 @@ def load_data(max_=None, load_entities=True):
     return frame
 
 
+def extract_features(df):
+    """
+    Extracts the features from the data.
+    
+    :param df:      pandas data frame containing the data
+    :return:        extracted features
+    """
+    pass
+
+
+#def word2features(sent, i):
+#    word = sent[i][0]
+#    postag = sent[i][1]
+#
+#    features = {
+#        'bias': 1.0,
+#        'word.lower()': word.lower(),
+#        'word[-3:]': word[-3:],
+#        'word.isupper()': word.isupper(),
+#        'word.istitle()': word.istitle(),
+#        'word.isdigit()': word.isdigit(),
+#        'postag': postag,
+#        'postag[:2]': postag[:2],
+#    }
+#    if i > 0:
+#        word1 = sent[i-1][0]
+#        postag1 = sent[i-1][1]
+#        features.update({
+#            '-1:word.lower()': word1.lower(),
+#            '-1:word.istitle()': word1.istitle(),
+#            '-1:word.isupper()': word1.isupper(),
+#            '-1:postag': postag1,
+#            '-1:postag[:2]': postag1[:2],
+#        })
+#    else:
+#        features['BOS'] = True
+#
+#    if i < len(sent)-1:
+#        word1 = sent[i+1][0]
+#        postag1 = sent[i+1][1]
+#        features.update({
+#            '+1:word.lower()': word1.lower(),
+#            '+1:word.istitle()': word1.istitle(),
+#            '+1:word.isupper()': word1.isupper(),
+#            '+1:postag': postag1,
+#            '+1:postag[:2]': postag1[:2],
+#        })
+#    else:
+#        features['EOS'] = True
+#
+#    return features
+#
+#
+#def sent2features(sent):
+#    return [word2features(sent, i) for i in range(len(sent))]
+    
 df = load_data(max_=10, load_entities=False)
 print(df)
